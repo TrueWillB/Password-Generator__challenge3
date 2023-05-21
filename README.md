4b1a19f02c788b5b71a7ace0017fdc3b94300cb7
William Brown <willis291@gmail.com>
2023-05-21 13:54:23 -0600

    Finished random number generator code. Password generator now working. Started adding to readme

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Password Generator Starter Code
+
+## Purpose
+
+This is a simple password generator, written as a first introduction to the use of javascript. The user will enter a password length and what types of characters to include in the password, and then a password will be randomly generated. It should be noted that this password that is generated is not necessarily cryptographically secure, as it uses the Math.random method to generate the randomized letters. The ideal circumstance would be to use the Crypto.getRandomValues to generate more secure randomly generated passwords. PLEASE USE THIS GENERATOR AT YOUR OWN RISK.
+
+## Notes
+
+- I added a feature that checks to make sure that at least 1 character of each type that the user specified is included in the generated password.
--- assets/script.js
+++ assets/script.js
@@ -5,10 +5,10 @@ var generateBtn = document.querySelector("#generate");
 
 //this 2-dimensional array is where I'm going to draw random characters from
 var passCharRefArray = [
-  ["abcdefghijklmnopqrstuvwxyz".split("")],
-  ["ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("")],
-  ["0123456789".split("")],
-  ["!\"#$%&'()*+,-./:;<=>?@[]^_`{|}~".split("")], //I made the specific decision to not include the space character. Including it would be a simple matter of just typing a space in this string of characters.
+  "abcdefghijklmnopqrstuvwxyz".split(""), //lowercase array, lInclude
+  "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split(""), // uppercase array, uInclude
+  "0123456789".split(""), //numerical array, nInclude
+  " !\"#$%&'()*+,-./:;<=>?@[]^_`{|}~".split(""), //sepcial character array, sInclude. You can change this string to decide what special characters are and are not allowed. I allowed all of them to start with
 ];
 
 // Write password to the #password input
@@ -25,14 +25,19 @@ function writePassword() {
     passLength = prompt(
       "Enter the desired character length of your password (8-128):"
     );
-
     if (passLength === null) {
       //exit with a message if you press the cancel button. Nothing more annoying than an endless prompt loop you want to leave
       alert("Operation canceled");
       return;
     }
+    passLength *= 1; //Changes passLength to a number type, allows to check later for NaN to sanitize inputs. Found with lots of experimentation
+    passLength = Math.floor(passLength); //Strips away decimals
+    //exits creation if passlength is not a number
+    if (isNaN(passLength)) {
+      alert("Please only enter numbers");
+      return;
+    }
   }
-
   while (criteriaCheck == false) {
     criteriaString = prompt(
       "Please type all criteria for your password. At least one criteria must be selected:\n" +
@@ -41,7 +46,6 @@ function writePassword() {
         "Type 'N' to include numerals\n" +
         "Type 'S' to include special characters"
     );
-
     if (criteriaString === null) {
       //exit with a message if you press the cancel button. Nothing more annoying than an endless prompt loop you want to leave
       alert("Operation canceled");
@@ -54,8 +58,6 @@ function writePassword() {
     nInclude = criteriaString.includes("N");
     sInclude = criteriaString.includes("S");
   }
-  // console.log("made it out of criteria check");
-
   //will pass all password criteria to the password generator to do the actual crunching
   var password = generatePassword(
     uInclude,
@@ -64,17 +66,9 @@ function writePassword() {
     sInclude,
     passLength
   );
-
   //actually insert password into text box
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
-}
-//THIS IS WHERE YOU LEFT OFF. Need to generate password. Try building a new array to pull from using the xInclude booleans, then iterate random number generators multiplied by each individual
-function generatePassword(uInclude, lInclude, nInclude, sInclude, passLength) {
-  // alert("entered 'generatePassword()'");
-  var finishedPassword = "00000000";
-  console.log([uInclude, lInclude, nInclude, sInclude, passLength]);
-  return finishedPassword;
 }
 
 //this just performs the conditional statement to make sure the user entered at least one option for password criteria
@@ -90,6 +84,60 @@ function criteriaChecker(inputString) {
     return false;
   }
 }
+
+function generatePassword(uInclude, lInclude, nInclude, sInclude, passLength) {
+  var finishedPassword = "";
+  var charPossibilityArray = ["intentionally left blank"]; //I couldn't find a way to just push elements onto an empty array, which is what I wanted to do in the first place. I decided instead to just make a burner array and then delete it later then unshift
+  if (sInclude === true) {
+    charPossibilityArray.unshift(passCharRefArray[3]);
+  }
+  if (nInclude === true) {
+    charPossibilityArray.unshift(passCharRefArray[2]);
+  }
+  if (uInclude === true) {
+    charPossibilityArray.unshift(passCharRefArray[1]);
+  }
+  if (lInclude === true) {
+    charPossibilityArray.unshift(passCharRefArray[0]);
+  }
+  charPossibilityArray.pop(); //delete the placeholder array character
+  var NumOfCharTypes = charPossibilityArray.length; //get the number of character types the user selected for looping purposes later
+  var containsAll = false;
+  var falseCounter = 0;
+  while (containsAll === false) {
+    var underConstructionPassword = ""; //empties out password under construction if it does not pass validity
+    var charTypePickedCheck = [false, false, false, false]; // This will be used to make sure that if the user selected a character type, it is indeed represented in the password
+
+    for (let i = 0; i < passLength; i++) {
+      var charTypeChoice = Math.floor(Math.random() * NumOfCharTypes);
+      var currentChar =
+        charPossibilityArray[charTypeChoice][
+          Math.floor(
+            Math.random() * charPossibilityArray[charTypeChoice].length
+          )
+        ];
+      underConstructionPassword += currentChar;
+      charTypePickedCheck[charTypeChoice] = true; //Flags in an array that a character type was picked.
+    }
+    //this loops through and checks to ensure that each character type was picked, checking to see if the entry in charTypePickedCheck is flagged as true
+    for (let i = 0; i < NumOfCharTypes; i++) {
+      if (charTypePickedCheck[i] === false) {
+        containsAll = false;
+        falseCounter++;
+        console.log(
+          "Invalid password generated, will try again:" +
+            underConstructionPassword
+        );
+        break;
+      }
+      containsAll = true;
+    }
+  }
+
+  finishedPassword = underConstructionPassword;
+  return finishedPassword;
+}
+
 //Here's where code actually starts executing===========================================================================
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
